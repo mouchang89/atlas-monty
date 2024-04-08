1fc415a7f28e8cd899ea29715b0af9e223439fdc
mouchang89 <mou.chang@atlasschool.com>
2024-04-08 03:09:21 +0000

    added ; to push fprintf and deleted 2nd if statement

--- monty.c
+++ monty.c
@@ -13,15 +13,10 @@ void push(stack_t **stack, unsigned int ln)
 
 	if (stack == NULL || *stack == NULL)
 	{
-		fprintf(stderr, "L%d: L<line_number>: usage: push integer\n", ln)
+		fprintf(stderr, "L%d: L<line_number>: usage: push integer\n", ln);
 		exit(EXIT_FAILURE);
 	}
-	if (head == NULL)
-	{
-		head = *new_node;
-		return;
-	}
-	tmp = atoi(*stack);
+	tmp = atoi(stack);
 	tmp = head;
 	head = *new_node;
 	head->next = tmp;
