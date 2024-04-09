f6efe922dd054e7f2489a2f06ee3c7a47d2eeb4a
mouchang89 <mou.chang@atlasschool.com>
2024-04-09 02:14:42 +0000

    removed fprintf statement

--- pop.c
+++ pop.c
@@ -2,21 +2,25 @@
 
 /**
  * pop - removes the top element of the stack
- * @stack: double pointer to stack
- * @ln: line number
+ * @stack: pointer to top of stack
+ * @line_number: current working line number
 *
  * Return: 0
  */
 void pop(stack_t **stack, unsigned int ln)
 {
-    	stack_t *tmp;
+    	stack_t *next = NULL;
 
-    	if (!*stack)
+    	if ((*stack)->next == NULL)
     	{
-            	fprintf(stderr, "L%d: can't pop an empty stack\n", ln);
-            	exit(EXIT_FAILURE);
+		set_op_tok_error(pop_error(line_number));
+		return;
     	}
-    	tmp = *stack;
-    	*stack = *stack->next;
-    	free(tmp);
+	next = (*stack)->next->next;
+	free((*stack)->next);
+	if (next)
+	{
+		next->prev = *stack;
+	}
+	(*stack)->next = next;
 }
